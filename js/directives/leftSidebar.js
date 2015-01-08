89e1ccf76b42e6f76819550ee56597a05dec9d19
luboslav1995 <milkovlubo@gmail.com>
2015-01-08 18:59:57 +0200

    changes

---
+++ js/directives/leftSidebar.js
@@ -0,0 +1,8 @@
+﻿app.directive('leftPublicSidebar', function () {
+    return {
+        controller: 'HomeController',
+        restrict: 'E',
+        templateUrl: 'templates/public/left-sidebar.html',
+        replace: true,
+    };
+});
