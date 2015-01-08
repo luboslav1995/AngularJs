22d5bc7fafa729677d8fa9d1061ee67ab727e6b1
luboslav1995 <milkovlubo@gmail.com>
2015-01-08 23:25:03 +0200

    hard work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 'use strict';
 
-var app = angular.module('adsApp', ['ngRoute']);
+var app = angular.module('adsApp', ['ngRoute', 'ngResource']);
+
+app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net/api/');
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/', {
         templateUrl: '/templates/home.html',
         controller: 'HomeController'
+    });
+    $routeProvider.when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController',
+    });
+    $routeProvider.when('/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
     });
     $routeProvider.otherwise({
         redirectTo: '/'
---
+++ js/services/data/userData.js
@@ -0,0 +1,20 @@
+﻿app.factory('userData', ['$resource', 'baseServiceUrl', function ($resource, baseServiceUrl) {
+
+    function registerUser(user) {
+        return $resource(baseServiceUrl + 'user/register')
+        .save(user);
+    }
+
+    function loginUser(user) {
+
+    }
+
+    function logoutUser() {
+
+    }
+    return {
+        register: registerUser,
+        login: loginUser,
+        logout: logoutUser
+    }
+}]);
